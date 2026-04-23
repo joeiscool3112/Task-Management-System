e073bf1ec5130311dad30603ffc17df32a0cf36c
Hung <joetranyt@gmail.com>
2026-04-23 22:17:56 +0700

    chore: add server.js and hashpassword

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
 // Cho phép tất cả (development)
 app.use(cors());
@@ -10,6 +11,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("backend is runnging");
 });
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
