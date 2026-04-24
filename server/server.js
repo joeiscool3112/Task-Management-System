435cfa7a351ed13acbac17d05b00514f933b1d2f
Hung <joetranyt@gmail.com>
2026-04-24 23:42:18 +0700

    chore: add CRUD projects (#36 37 38 39)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const PORT = 3000;
 app.use(express.json());
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
+const projectsRoutes = require("./routes/projects");
+const tasksRoutes = require("./routes/tasks");
+
 
 // Cho phép tất cả (development)
 app.use(cors());
@@ -13,6 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectsRoutes);
+app.use("/api/projects", tasksRoutes);
+
 
 
 app.listen(PORT, () => {
