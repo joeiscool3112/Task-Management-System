a59630b90c17742bce82f5d83c6333656e0a7b97
Hung <joetranyt@gmail.com>
2026-04-27 15:30:13 +0700

    feat: add login and register (#29 #30 #31 #32)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,219 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
 import './App.css'
+import RegisterForm from './RegisterForm'
+import LoginForm from './LoginForm'
+import API_URL from './config'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [registerEmail, setRegisterEmail] = useState('');
+const [registerPassword, setRegisterPassword] = useState('');
+const [registerUsername, setRegisterUsername] = useState('');
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+const [loginEmail, setLoginEmail] = useState('');
+const [loginPassword, setLoginPassword] = useState('');
+const [registerError, setRegisterError] = useState("");
+const [loginError, setLoginError] = useState("");
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+const [usercreated, setUserCreated] = useState(false);
+const [username, setUsername] = useState("");
+const [activeTab, setActiveTab] = useState("register");
+const [books, setBooks] = useState([]);
+const [title, setTitle] = useState("");
+const [genre, setGenre] = useState("");
+const [error, setError] = useState("");
+useEffect(() => {
+  const checkToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    try {
+      const res = await fetch(`${API_URL}/api/auth/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        localStorage.removeItem("token");
+        setIsLoggedIn(false);
+        return;
+      }
+
+      if (data.user) {
+        setIsLoggedIn(true);
+      }
+    } catch (err) {
+      localStorage.removeItem("token");
+      setIsLoggedIn(false);
+    }
+  };
+
+  checkToken();
+}, []);
+
+
+
+async function handleSubmitRegister(e) {
+  e.preventDefault();
+  setRegisterError("");
+
+  if (!registerUsername || !registerEmail || !registerPassword) {
+    setRegisterError("All fields are required");
+    return;
+  }
+
+  if (registerPassword.length < 6) {
+    setRegisterError("Password must be at least 6 characters");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/api/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: registerUsername,
+        email: registerEmail,
+        password: registerPassword,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setRegisterError(data.error);
+      return;
+    }
+
+    localStorage.setItem("token", data.token);
+    setIsLoggedIn(true);
+    setUsername(data.username)
+    setRegisterError("");
+  } catch (err) {
+    console.error("REGISTER ERROR:", err);
+    setRegisterError("Server error");
+  }
+}
+
+async function handleSubmitLogin(e) {
+  e.preventDefault();
+  setLoginError("");
+
+  if (!loginEmail || !loginPassword) {
+    setLoginError("Email and password are required");
+    return;
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: loginEmail, password: loginPassword }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setLoginError(data.error);
+      return;
+    }
+
+    localStorage.setItem("token", data.token);
+    setIsLoggedIn(true);
+    console.log(data)
+    setUsername(data.user.username);
+    setLoginError("");
+  } catch (err) {
+    setLoginError("Server error");
+  }
+}
+  
+ return (
+  <div className="app-shell">
+    {isLoggedIn && (
+      <>
         <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          className="top-action-btn"
+          onClick={() => {
+            localStorage.removeItem("token");
+            setIsLoggedIn(false);
+          }}
         >
-          Count is {count}
+          {username} - Logout
         </button>
-      </section>
 
-      <div className="ticks"></div>
+      
+      </>
+    )}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+    {!isLoggedIn && (
+      <>
+        <div className="tab-row">
+          <button onClick={() => setActiveTab("register")}>
+            Register
+          </button>
+          <button onClick={() => setActiveTab("login")}>
+            Login
+          </button>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+        {activeTab === "register" ? (
+          <button
+            className="switch-btn"
+            onClick={() => {
+              setUserCreated(true);
+              setActiveTab("login");
+            }}
+          >
+            Already have an account? Login here
+          </button>
+        ) : (
+          <button
+            className="switch-btn"
+            onClick={() => {
+              setUserCreated(false);
+              setActiveTab("register");
+            }}
+          >
+            Don't have an account? Register here
+          </button>
+        )}
+
+        {activeTab === "register" ? (
+          <>
+            <RegisterForm 
+              email={registerEmail} 
+              password={registerPassword} 
+              setEmail={setRegisterEmail} 
+              setPassword={setRegisterPassword} 
+              setUsername={setRegisterUsername}
+              username={registerUsername}
+              handleSubmitRegister={handleSubmitRegister}
+            />
+            {registerError && <p className="error-text">{registerError}</p>}
+          </>
+        ) : (
+          <>
+            <LoginForm
+              email={loginEmail} 
+              password={loginPassword} 
+              setEmail={setLoginEmail} 
+              setPassword={setLoginPassword} 
+              handleSubmitLogin={handleSubmitLogin}
+            />
+            {loginError && <p className="error-text">{loginError}</p>}
+          </>
+        )}
+      </>
+    )}
+  </div>
+);
 }
 
 export default App
