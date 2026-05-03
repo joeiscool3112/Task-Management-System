c74373867401a739938ad04826ce7c567c364a86
Hung <joetranyt@gmail.com>
2026-05-03 21:46:08 +0700

    fix render server config

--- server/server.js
+++ server/server.js
@@ -1,34 +1,31 @@
-require("dontenv").config();
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
+
 const authRoutes = require("./routes/auth");
 const projectsRoutes = require("./routes/projects");
 const tasksRoutes = require("./routes/tasks");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Cho phép tất cả (development)
-aapp.use(
+app.use(express.json());
+
+app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
   })
 );
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("backend is runnging");
+  res.send("backend is running");
 });
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectsRoutes);
 app.use("/api/projects", tasksRoutes);
 
-
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
