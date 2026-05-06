afd533890ea04907c2e5ae79ad27fd027e0ede13
Hung <joetranyt@gmail.com>
2026-05-06 15:57:06 +0700

    fixed cors

--- server/server.js
+++ server/server.js
@@ -12,11 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//   })
-// );
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("backend is running");
